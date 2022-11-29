8d49283600ce9186919b2f3bf4eeda0e74bf474c
Puzerevich <puzerevich@gmail.com>
2022-11-29 09:32:14 +0200

    Create ArrayValueCalculator.java

---
+++ src/ArrayValueCalculator.java
@@ -0,0 +1,32 @@
+import Exception.ArraySizeException;
+import Exception.ArrayDataException;
+
+import java.util.Arrays;
+
+public class ArrayValueCalculator {
+
+    static int doCalc(String[][] str) throws ArraySizeException, ArrayDataException {
+        int sum =0;
+//        String sumStr ="";
+        int[] beg ={0,0};
+
+        if (str.length == 4 && str[0].length == 4) {
+            try {
+                for (int i = 0; i < str.length; i++) {
+                    beg[0] = i;
+                    for (int j = 0; j < str[0].length; j++) {
+                        beg[1] = j;
+                        sum += Integer.parseInt(str[i][j]);
+//                        sumStr += str[i][j] + ", ";
+                    }
+                }
+            }catch (NumberFormatException e) {
+                throw new ArrayDataException("Error on line " + Arrays.toString(str));
+            }
+        } else {
+            throw new ArraySizeException(String.format("Error size. Your array is %dx%d", str.length, str[0].length));
+        }
+
+        return  sum;
+    }
+}
